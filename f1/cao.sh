542132e44cc79c437d8f07cc060eaa232a20493e
fpimenta <fpimenta@fpimenta.com>
2018-02-07 15:34:53 +0000

    mod cao

--- f1/cao.sh
+++ f1/cao.sh
@@ -1,1 +1,3 @@
 O cão (Canis lupus familiaris), no Brasil também chamado de cachorro, é um mamífero carnívoro da família dos canídeos, subespécie do lobo, e talvez o mais antigo animal domesticado pelo ser humano. Teorias postulam que surgiu do lobo cinzento no continente asiático há mais de 100 000 anos. Ao longo dos século
+
+plokijiuhuj
