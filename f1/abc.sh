3a048add747d86b2e73cc8dfc483e06c441a4b54
fpimenta <fpimenta@fpimenta.com>
2018-02-07 15:13:57 +0000

    2 commit

---
+++ f1/abc.sh
@@ -0,0 +1,8 @@
+2011-06-03:
+Minor bug fix:
+Parallel decomposing no longer omits the initial directory
+2011-07-01: Overhauled documentation; no change to code
+2012-10-25: Version 0.5.0
+Moved to git
+Enabled boundary switching
+Introduced version nu
---
+++ f2/gtao/gat.py
@@ -0,0 +1,1 @@
+Nos casos de cães desaparecidos, a ajuda de terceiros é quase sempre essencial. Pense em todas as pessoas que vivem dentro do círculo, pessoas que passem tempo fora de casa e que seja provável repararem num cão na rua. Pense nas pessoas que percorrem as ruas como parte do seu trabalho, pessoas que você .
